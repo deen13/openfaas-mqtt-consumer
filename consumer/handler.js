14280e2500f8c145de3684b7f9e65a757702023e
deen13 <dierkes@smartsquare.de>
2020-11-05 14:49:48 +0100

    Add consumer

---
+++ consumer/handler.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+module.exports = async (event, context) => {
+  const result = {
+    'status': 'Received input: ' + JSON.stringify(event.body)
+  }
+
+  await delay(1000);
+
+  console.log(result)
+
+  return context.status(200)
+}
